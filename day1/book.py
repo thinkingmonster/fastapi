4a698ac04feefb019175b9d22e91356a6036dd94
Akashdeep Thakur <akashdeept@adobe.com>
2025-12-11 08:11:04 +0530

    Day 3: Build DB model for Todo APP

--- day1/book.py
+++ day1/book.py
@@ -4,12 +4,12 @@ app = FastAPI()
 
 
 BOOKS = [
-    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
-    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
-    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
-    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"},
 ]
 
 
@@ -21,7 +21,7 @@ async def read_all_books():
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
-        if book.get('title').casefold() == book_title.casefold():
+        if book.get("title").casefold() == book_title.casefold():
             return book
 
 
@@ -29,7 +29,7 @@ async def read_book(book_title: str):
 async def read_category_by_query(category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('category').casefold() == category.casefold():
+        if book.get("category").casefold() == category.casefold():
             books_to_return.append(book)
     return books_to_return
 
@@ -39,7 +39,7 @@ async def read_category_by_query(category: str):
 async def read_books_by_author_path(author: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == author.casefold():
+        if book.get("author").casefold() == author.casefold():
             books_to_return.append(book)
 
     return books_to_return
@@ -49,8 +49,10 @@ async def read_books_by_author_path(author: str):
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and \
-                book.get('category').casefold() == category.casefold():
+        if (
+            book.get("author").casefold() == book_author.casefold()
+            and book.get("category").casefold() == category.casefold()
+        ):
             books_to_return.append(book)
 
     return books_to_return
@@ -64,13 +66,13 @@ async def create_book(new_book=Body()):
 @app.put("/books/update_book")
 async def update_book(updated_book=Body()):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+        if BOOKS[i].get("title").casefold() == updated_book.get("title").casefold():
             BOOKS[i] = updated_book
 
 
 @app.delete("/books/delete_book/{book_title}")
 async def delete_book(book_title: str):
     for i in range(len(BOOKS)):
-        if BOOKS[i].get('title').casefold() == book_title.casefold():
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
             BOOKS.pop(i)
             break
